ca4738e5fb7acdc543f43c753baa11459a1a0d2b
deoxxa <deoxxa@fknsrs.biz>
2014-02-22 20:20:33 +1100

    refactor cli program to use proper argument parser

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ module.exports = require("./lib/jesus");
 var Daemon = module.exports.Daemon = require("./lib/daemon");
 var Client = module.exports.Client = require("./lib/client");
 
-module.exports.createServer = function createServer() {
-  var daemon = new Daemon();
-
-  return daemon;
+module.exports.createServer = function createServer(options) {
+  return new Daemon(options);
 };
 
-module.exports.connect = function connect(port, host) {
-  return Client.connect(port, host);
+module.exports.connect = function connect(port, host, cb) {
+  return Client.connect(port, host, cb);
 };
